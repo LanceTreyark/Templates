ebe0636dabbe6356397bc435a0302e761469cbbe
LanceTreyark <github@treyark.com>
2022-12-28 06:19:57 -0800

    first commit

---
+++ fragmentTesting.sh
@@ -0,0 +1,287 @@
+#!/bin/bash
+# Code by, Lance Pierson
+# TreyarkMedia
+# Website: treyark.com | Twitter: @LanceTreyark
+
+# This is a template for Fragment Testing
+
+# "Fragment Testing" is a term I use for taking a larege script and breaking it up into tiny fragments to test the
+# integrity of the commands
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
+#-------------------------------PAUSE------------IN
+sleep 1
+echo " "
+echo ""
+echo "Press Enter to continue running the script"
+read -s -p ""
+echo "Continuing script..."
+echo " "
+#-------------------------------PAUSE------------OUT
+
+
+
